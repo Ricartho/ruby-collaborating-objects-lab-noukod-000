679df659267566a696105512925331c97e97a1ad
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-25 20:46:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,8 +11,10 @@ class Song
   end 
   
   def self.new_by_filename(file_name)
+    
     file_info = file_name.chomp(".mp3").split(" - ")
     new_song = self.new(file_info[1])
+    
     new_song.artist_name = file_info[0]
     new_song
     
