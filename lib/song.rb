09485d067f3c384c113506c76d927fcfb800492a
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-25 20:31:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,10 +5,14 @@ class Song
     @name = name
   end 
   
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end 
   def self.new_by_filename(file_name)
     file_info = file_name.chomp(".mp3").split(" - ")
     new_song = self.new(file_info[1])
-    
+    new_song.artist_name = file_info[0]
     new_song
     
   end 
