a34d125bced44ff3c6a8cfaf9e338d3ce5ec85be
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-25 20:11:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,20 +9,9 @@ class Song
     file_info = file_name.chomp(".mp3").split(" - ")
     new_song = self.new(file_info[1])
     
-    
   end 
   
-  # def self.all
-  #   @@all
-  # end 
-  
-  # def artist_name
-  #   if self.artist
-  #       self.artist.name 
-  #   else 
-  #       return nil 
-  #   end 
-  # end 
+ 
   
   
 end 
