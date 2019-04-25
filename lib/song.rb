0ca38a96eb670e4e782980c27446aa5c0e6f5fd3
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-25 19:51:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,10 +7,9 @@ class Song
   end 
   
   def self.new_by_filename(file_name)
-  
     title = file_name.split(" - ")[1]
     new_song = self.new(title)
-    
+    artist_name = file_name.split(" - ")[0] 
   end 
   
   # def self.all
